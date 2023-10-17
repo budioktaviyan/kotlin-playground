8cf3d58bbc43292460852f0267b6609b288847a0
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-10-18 00:01:05 +0700

    [COROUTINES] Synchronous Code

--- src/main/kotlin/id/kotlin/playground/Main.kt
+++ src/main/kotlin/id/kotlin/playground/Main.kt
@@ -1,4 +1,27 @@
 package id.kotlin.playground
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
 fun main() {
+  val time = measureTimeMillis {
+    runBlocking {
+      println("Weather forecast")
+      printForecast()
+      printTemperature()
+    }
+  }
+
+  println("Execution time: ${time / 1000.0} seconds")
+}
+
+suspend fun printForecast() {
+  delay(1000)
+  println("Sunny")
+}
+
+suspend fun printTemperature() {
+  delay(1000)
+  println("30\u00b0C")
 }
