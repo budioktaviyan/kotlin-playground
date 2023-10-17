c9355b505dc0f4b6037901c1d9751893a538d809
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-10-18 00:11:25 +0700

    [COROUTINES] Asynchronous Code

--- src/main/kotlin/id/kotlin/playground/Main.kt
+++ src/main/kotlin/id/kotlin/playground/Main.kt
@@ -1,27 +1,30 @@
 package id.kotlin.playground
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlin.system.measureTimeMillis
 
 fun main() {
-  val time = measureTimeMillis {
-    runBlocking {
-      println("Weather forecast")
-      printForecast()
-      printTemperature()
-    }
+  runBlocking {
+    println("Weather forecast")
+    println(getWeatherReport())
+    println("Have a good day!")
   }
-
-  println("Execution time: ${time / 1000.0} seconds")
 }
 
-suspend fun printForecast() {
-  delay(1000)
-  println("Sunny")
+suspend fun getWeatherReport() = coroutineScope {
+  val forecast = async { getForecast() }
+  val temperature = async { getTemperature() }
+  "${forecast.await()} ${temperature.await()}"
 }
 
-suspend fun printTemperature() {
+suspend fun getForecast(): String {
   delay(1000)
-  println("30\u00b0C")
+  return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+  delay(1000)
+  return "30\u00b0C"
 }
