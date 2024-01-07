52208bd544fa214c53582e0b3e2f5b46d5654a55
Budi Oktaviyan <budioktaviyans@gmail.com>
2024-01-08 00:12:35 +0700

    [COROUTINES] Coroutine Concepts

--- src/main/kotlin/id/kotlin/playground/Main.kt
+++ src/main/kotlin/id/kotlin/playground/Main.kt
@@ -1,34 +1,27 @@
 package id.kotlin.playground
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 fun main() {
   runBlocking {
-    println("Weather forecast")
-    println(getWeatherReport())
-    println("Have a good day!")
+    println("${Thread.currentThread().name} - runBlocking function")
+
+    launch {
+      println("${Thread.currentThread().name} - launch function")
+
+      withContext(Dispatchers.Default) {
+        println("${Thread.currentThread().name} - withContext function")
+        delay(1000)
+        println("10 results found.")
+      }
+
+      println("${Thread.currentThread().name} - end of launch function")
+    }
+
+    println("Loading...")
   }
-}
-
-suspend fun getWeatherReport() = coroutineScope {
-  val forecast = async { getForecast() }
-  val temperature = async { getTemperature() }
-
-  delay(200)
-  temperature.cancel()
-
-  "${forecast.await()}"
-}
-
-suspend fun getForecast(): String {
-  delay(1000)
-  return "Sunny"
-}
-
-suspend fun getTemperature(): String {
-  delay(1000)
-  return "30\u00b0C"
 }
